48bbe554c1b06038c305dbbcc8b3580d68a7254f
Saveen Reddy <saveenr@live.com>
2018-11-13 09:30:51 -0800

    NuGet package builds script now automatically updtes the packae versions and inter-dependency versions to be consistent. This removes a source of manual error when building the packages.

--- GraphLayout/NuGet/build_nuget_packages.ps1
+++ GraphLayout/NuGet/build_nuget_packages.ps1
@@ -1,8 +1,69 @@
+param([string]$ver="1.1.3") 
+
 .\AcquireNuGetExe.ps1
 
+$tokens = $ver.Split(".")
+
+
+if ($ver.Length -eq $null)
+{
+    Write-Error "Version cant't be null"
+    exit
+}
+
+if ($ver.Length -lt 5)
+{
+    Write-Error "Version must have at least 5 total chars"
+    exit
+}
+
+if ($tokens.Count -ne 3)
+{
+    Write-Error "version must have three components"
+    exit
+}
+
+
+$specs = ("Microsoft.Msagl.nuspec",
+    "Microsoft.Msagl.Drawing.nuspec",
+    "Microsoft.Msagl.GraphViewerGDI.nuspec",
+    "Microsoft.Msagl.WpfGraphControl.nuspec")
+
+
 Push-Location $PSScriptRoot 
-nuget pack Microsoft.Msagl.nuspec
-nuget pack Microsoft.Msagl.Drawing.nuspec
-nuget pack Microsoft.Msagl.GraphViewerGDI.nuspec
-nuget pack Microsoft.Msagl.WpfGraphControl.nuspec
+
+foreach ($spec in $specs)
+{
+    Write-Host "----------"
+
+    $spec_fname = Join-Path $PSScriptRoot $spec
+
+    $xml = [xml](Get-Content $spec_fname)
+
+    # Update the package version
+    Write-Host $xml.package.metadata.version
+    $xml.package.metadata.version = $ver
+    Write-Host $xml.package.metadata.version
+
+    # Updating the package versions of Msagl depedencies
+
+    foreach ($dep in $xml.package.metadata.dependencies.dependency)
+    {
+        if ($dep.id.Contains("Microsoft.Msagl"))
+        {
+            Write-Host $dep.id $dep.version
+            $dep.version = $ver
+            Write-Host $dep.id $dep.version
+        }
+    } 
+
+    $xml.Save( $spec_fname )
+}
+
+foreach ($spec in $specs)
+{
+    Write-Host "----------"
+    Write-Host "NUSPEC:" $spec_fname
+    #Invoke-Expression "nuget pack $spec"
+}
 Pop-Location
